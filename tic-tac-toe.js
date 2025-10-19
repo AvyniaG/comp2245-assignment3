6be80ee83d43fefc12a042efd3f7efc8abd96e0a
AvyniaG <500002157@my.fiveislands.uwi.edu>
2025-10-18 23:31:27 -0400

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   let squares = document.querySelectorAll("#board div");
+  let currentPlayer = "X";
 
   for (let i = 0; i < squares.length; i++) {
     squares[i].classList.add("square");
+
+    squares[i].addEventListener("click", function() {
+      if (squares[i].textContent === "") {
+        squares[i].textContent = currentPlayer;
+        squares[i].classList.add(currentPlayer);
+
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+      }
+    });
   }
 });
